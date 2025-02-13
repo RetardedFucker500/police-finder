48e6bb9630e59303931f2d73bfa33b7e66cf9a44
RetardedFucker500 <prodleland67@gmail.com>
2025-02-13 13:31:49 -0500

    push

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const axios = require("axios");
+
+const app = express();
+
+const ip = '0.0.0.0';
+const port = 8080;
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+const server = http.createServer(app);
+
+server.listen(ip, port);
